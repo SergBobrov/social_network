9634dd8c6a85f6e171a0f2757858a8a171d41b56
Serg <serggbobrov@gmail.com>
2020-08-20 13:36:21 +0300

    Import

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (<div>
+        <a href="#">Home</a>
+        <a href="#">News Feed</a>
+        <a href="#">Messages</a>
+    </div>)
+};
---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+};
